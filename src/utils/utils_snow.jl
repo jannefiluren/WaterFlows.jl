d47e03acb81601c5e01a3c74560f39ddb787a057
jannefiluren <jan.magnusson@gmail.com>
2019-08-09 12:35:56 +0200

    added refreeze and description

--- src/utils/utils_snow.jl
+++ src/utils/utils_snow.jl
@@ -1,3 +1,9 @@
+# Kavetski, D., and G. Kuczera (2007), Model smoothing strategies to remove microscale
+# discontinuities and spurious secondary optima in objective functions in hydrological
+# calibration, Water Resour. Res., 43, W03411, doi:10.1029/2006WR005195.
+# 
+# See Equation 8 for precipitation, and Equation 13 for melting/refreezing
+
 @inline function split_prec(prec, tair, tth_phase = 0.0; m_phase = 0.5)
     
     frac_snowfall = 1. / (1. + exp( (tair - tth_phase) / m_phase ))
@@ -15,5 +21,14 @@ end
     pot_melt = ddf * m_melt * (t_m + log(1. + exp(-t_m)))
     
     return pot_melt
+    
+end
+
+@inline function pot_refreeze(tair, drf = 3.0, tth_refreeze = 0.0; m_refreeze = 0.5)
+    
+    t_m = -(tair - tth_refreeze) / m_refreeze
+    pot_refreeze = drf * m_refreeze * (t_m + log(1. + exp(-t_m)))
+    
+    return pot_refreeze
     
 end
